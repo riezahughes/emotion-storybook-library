3d88b42ff9c7ef73e4473c148b818a5485ca08de
Dan Ruxton <dan@ekwholesale.co.uk>
2022-05-02 18:33:50 +0000

    Vite setup for storybook

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      jscolor: path.resolve("./util/jscolor.js"),
+      app: path.resolve("./app"),
+      css: path.resolve("./css"),
+      util: path.resolve("./util"),
+    },
+  },
+  plugins: [react()],
+});
