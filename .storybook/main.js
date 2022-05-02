7453454fda2d2ed0cfb10b48500edccd86d9becb
Dan Ruxton <dan@ekwholesale.co.uk>
2022-05-02 18:46:08 +0000

    sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,15 @@
+const { mergeConfig } = require("vite");
+
 module.exports = {
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      resolve: {
+        alias: { foo: "bar" },
+      },
+    });
+  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   core: { builder: "@storybook/builder-vite" },
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
